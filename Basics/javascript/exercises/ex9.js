6f58eee4f34a4a7c0c3f7828004df0b1ab44d39d
atiilla <9992685+atiilla@users.noreply.github.com>
2023-05-03 09:00:02 +0200

    exercise 9

---
+++ Basics/javascript/exercises/ex9.js
@@ -0,0 +1,8 @@
+// Find common elements between two arrays.
+
+// Define a function that takes two arrays as arguments and returns an array of common elements between both arrays.
+
+// Example:
+var arr1 = [1, 2, 3, 4, 5, 6];
+var arr2 = [2, 4, 6, 8, 10, 12];
+// Expected Output: [2, 4, 6]
