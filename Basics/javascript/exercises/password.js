c2ec47c35199478e7d0b49f9a95a23a8b030cf60
atiilla <9992685+atiilla@users.noreply.github.com>
2023-05-03 15:31:58 +0200

    password generate

---
+++ Basics/javascript/exercises/password.js
@@ -0,0 +1,20 @@
+// password generator
+
+// this is the array of characters that can be used to generate the password
+let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=";
+
+// create a function GeneratePassword(size) that takes a number as argument for length of password 
+    /*
+    * in the function, create a variable to store the password.
+    * create a loop that runs the number of times as the length of password.
+    * inside the loop, generate a random number between 0 and the length of chars.
+    * use the random number to get a character from chars and add it to the password variable.
+    * return the password variable.
+    */
+
+
+// create another function that takes a number as argument for how many passwords you want to generate
+    /*
+    * create a loop that invokes the GeneratePassword function the number of times as the number of passwords you want to generate.
+    * print the password to the console.
+    */
