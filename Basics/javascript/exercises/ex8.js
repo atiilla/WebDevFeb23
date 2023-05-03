a37ad2a709df0a5f0f2da7f8405b5b98b183b7d5
atiilla <9992685+atiilla@users.noreply.github.com>
2023-05-03 08:50:10 +0200

    exercise 8

---
+++ Basics/javascript/exercises/ex8.js
@@ -0,0 +1,12 @@
+// Write a function that returns the factorial of a number
+/*
+    5! = 5*4*3*2*1 > 120
+    methods required:
+        - Array.from()
+        - Array.forEach()
+*/
+function factorial(num){
+    
+}
+
+console.log(factorial(5))
---
+++ Basics/javascript/exercises/findAvg.js
@@ -0,0 +1,87 @@
+const students = [
+    {
+        "_id": "63c993feb6f5dc691c2c0ac3",
+        "name": "Fannie Buckley",
+        "scores": [
+            44,
+            72,
+            37
+        ]
+    },
+    {
+        "_id": "63c993fe0b604587275fbfdf",
+        "name": "Sherry Petty",
+        "scores": [
+            50,
+            85,
+            61
+        ]
+    },
+    {
+        "_id": "63c993fef0a0425682c58b2c",
+        "name": "Bray Lancaster",
+        "scores": [
+            37,
+            74,
+            56
+        ]
+    },
+    {
+        "_id": "63c993fe0115dd572cb63892",
+        "name": "Hubbard Munoz",
+        "scores": [
+            99,
+            40,
+            57
+        ]
+    },
+    {
+        "_id": "63c993fe9f8ad8038fc76416",
+        "name": "Copeland Ford",
+        "scores": [
+            66,
+            77,
+            53
+        ]
+    },
+    {
+        "_id": "63c993fef4067267e74d8231",
+        "name": "Padilla Bolton",
+        "scores": [
+            65,
+            98,
+            39
+        ]
+    },
+    {
+        "_id": "63c993fe187dd671983e006d",
+        "name": "Mcknight Odonnell",
+        "scores": [
+            97,
+            92,
+            40
+        ] // (97+92+40)/3
+    },
+    {
+        "_id": "63c993fe2ecf8e3dc2bd5393",
+        "name": "Bradley Delacruz",
+        "scores": [
+            57,
+            81,
+            87
+        ]
+    },
+    {
+        "_id": "63c993fefa0735736af48d92",
+        "name": "Velazquez Hensley",
+        "scores": [
+            67,
+            45,
+            52
+        ]
+    }
+]
+
+// Get avg result of each student in the array
+
+// and print who passed who's not
