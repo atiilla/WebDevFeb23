61ec44bb4911e1fbfc319495f6ca942b070877da
atiilla <9992685+atiilla@users.noreply.github.com>
2023-05-08 15:55:00 +0200

    diceroll

--- Basics/javascript/exercises/diceRoll.js
+++ Basics/javascript/exercises/diceRoll.js
@@ -1,7 +1,13 @@
 // target elements 
 const dice1 = document.getElementById('dice1')
 const dice2 = document.getElementById('dice2')
+const didUserWin = document.getElementById('diduserwin')
 
 function shakeIt(){
+    // get 2 different number (must be int)
+    // create control flow and verify that 2 numbers are equal
+    // if 2 numbers are equal then player won
+    // if not then print a message 'try again'
+    // to create random number, use  (Math) object
     console.log('hello')
 }
