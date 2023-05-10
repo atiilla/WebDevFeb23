5c777c50c5596ba3b18384ff011632bf45837f57
atiilla <9992685+atiilla@users.noreply.github.com>
2023-05-10 13:39:35 +0200

    todo html template created

--- Basics/javascript/oop/es5/todolist/index.js
+++ Basics/javascript/oop/es5/todolist/index.js
@@ -14,6 +14,15 @@
     - Owner/Author
 */
 
+// elements
+const tasksList = document.getElementById('tasksList')
+const taskTitle = document.getElementById('taskTitle')
+const taskDeadline = document.getElementById('taskDeadline')
+const taskCategory = document.getElementById('taskCategory')
+const taskDesc = document.getElementById('taskDesc')
+const isDone = document.getElementById('isDone')
+const taskOwner = document.getElementById('taskOwner')
+
 // Make an object constructor for App
 function TodoApplication() {
     this.todos = [] // all my todos/tasks
@@ -23,22 +32,38 @@ function TodoApplication() {
 
 // Show or Print all tasks
 TodoApplication.prototype.displayAllTasks = function () {
+
+    this.todos.forEach(task => {
+        tasksList.innerHTML += `
+        <tr>
+                            <td>${task.title}</td>
+                            <td>${task.created_date}</td>
+                            <td>${task.deadline}</td>
+                            <td>${task.category}</td>
+                            <td>${task.description}</td>
+                            <td>${task.isDone}</td>
+                            <td>${task.author}</td>
+                        </tr>
+        `
+    })
     return this.todos
 }
 
 // Add new task method
-TodoApplication.prototype.addTask = function (title, created_date, deadline, category, description, isDone = false, author) {
+TodoApplication.prototype.addTask = function (title, deadline, category, description, isDone = false, author) {
     // i need to insert new task into todos list
     let obj = {
         id: this.todos[this.todos.length - 1] ? this.todos[this.todos.length - 1].id + 1 : 1,
         title,
-        created_date,
+        created_date: `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`,
         deadline,
         category,
         description,
         isDone,
         author
     }
+
+
     return this.todos.push(obj)
 }
 
@@ -47,20 +72,20 @@ TodoApplication.prototype.removeTask = function (id) {
     let taskID = this.todos.findIndex(function (task) {
         return task.id === id // return id of the object/task
     })
-   if(taskID!==-1){
-    return this.todos.splice(taskID, 1)
-   }else{
-    console.log('Invalid ID')
-   }
+    if (taskID !== -1) {
+        return this.todos.splice(taskID, 1)
+    } else {
+        console.log('Invalid ID')
+    }
 }
 
 // Update a task
-TodoApplication.prototype.updateTask = function (id,newObject) {
+TodoApplication.prototype.updateTask = function (id, newObject) {
     let taskID = this.todos.findIndex(function (task) {
         return task.id === id // return id of the object/task
     })
-    newObject.id=id // new object includes ID
-   
+    newObject.id = id // new object includes ID
+    newObject.created_date = `${new Date().getDay()}/${new Date().getMonth()}/${new Date().getFullYear()}`
     // taskID is position number of the object in the todos array
 
     return this.todos[taskID] = newObject // update current object with "newObject"
@@ -72,13 +97,13 @@ TodoApplication.prototype.removeAll = function () {
     //     this.removeTask(id)
     //    })
     let i = 0
-    while(this.todos.length>0){
+    while (this.todos.length > 0) {
         this.todos.pop()
     }
 }
 
-TodoApplication.prototype.removeSelectedTasks = function(...ids){
-    ids.forEach(objId=>{
+TodoApplication.prototype.removeSelectedTasks = function (...ids) {
+    ids.forEach(objId => {
         this.removeTask(objId)
     })
 }
@@ -86,26 +111,61 @@ TodoApplication.prototype.removeSelectedTasks = function(...ids){
 
 let app = new TodoApplication();
 
-app.addTask('learn javascript', 'asd', 'asd', 'asd', 'asdsda', false, 'jackie')
-app.addTask('learn nodejs', 'asd', 'asd', 'asd', 'asdsda', false, 'jackie')
-app.addTask('learn react.js', 'asd', 'asd', 'asd', 'asdsda', false, 'jackie')
 
+function clickHandler() {
+    tasksList.innerHTML = ""
+    let BelgDateFormat = new Date(taskDeadline.value).toLocaleString('nl-NL').split(" ")[0]
+    console.log(BelgDateFormat)
+
+
+
+    if (isDone.options[isDone.selectedIndex].value.includes('false') || isDone.options[isDone.selectedIndex].value.includes('true')) {
+        let isDoneValue = JSON.parse(isDone.options[isDone.selectedIndex].value)
+        let owner = taskOwner.options[taskOwner.selectedIndex].value
+
+        // taskTitle.value
+        app.addTask(taskTitle.value, taskDeadline.value, taskCategory.value, taskDesc.value, isDoneValue, owner)
+
+        Swal.fire({
+            title: 'Success!',
+            text: 'Task is added!',
+            icon: 'success',
+            confirmButtonText: 'Understand!'
+        })
+
+        app.displayAllTasks()
+    } else {
+        // alert('You can not leave input field blank')
+        Swal.fire({
+            title: 'Error!',
+            text: 'You can not leave input field blank',
+            icon: 'error',
+            confirmButtonText: 'Understand!'
+        })
+    }
+
+
+
+
+    // remove inputs values
+
+}
 
 // app.removeTask(3)
 // app.removeTask(1)
 // app.removeTask(2)
 // app.removeAll()
-let allTasks = app.displayAllTasks()
-app.updateTask(3,{
-    title:'Learn Next.js',
-    created_date:'10 May',
-    deadline:'10 June',
-    category:'Fullstack',
-    description:'React framework',
-    isDone:false,
-    author:'admin'
+
+app.updateTask(3, {
+    title: 'Learn Next.js',
+    deadline: '10 June',
+    category: 'Fullstack',
+    description: 'React framework',
+    isDone: false,
+    author: 'admin'
 })
 
-app.removeSelectedTasks(2,3,1)
-console.log(allTasks)
+// app.removeSelectedTasks(2,3,1)
+// console.log(allTasks)
 
+app.displayAllTasks()
