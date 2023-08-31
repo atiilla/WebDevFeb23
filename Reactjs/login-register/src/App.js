0426142a0297b0adc5da55882a635d31f52817fa
atiilla <9992685+atiilla@users.noreply.github.com>
2023-08-31 14:23:48 +0200

    react toastify added

--- Reactjs/login-register/src/App.js
+++ Reactjs/login-register/src/App.js
@@ -4,11 +4,15 @@ import { Tab, Tabs } from 'react-bootstrap'
 import LoginForm from './components/auth/login';
 import RegisterForm from './components/auth/register';
 import Profile from './components/profile';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify';
+
 function App() {
     const [key, setKey] = useState('login');
     const [user, setUser] = useState({});
     const [loggedIn, setLoggedIn] = useState(false)
-    const [token,setToken] = useState("")
+    const [token, setToken] = useState("")
 
 
     useEffect(() => {
@@ -22,7 +26,7 @@ function App() {
             .catch(e => {
                 setLoggedIn(false)
             })
-    }, [loggedIn,token])
+    }, [loggedIn, token])
 
     const handleChange = e => {
         e.preventDefault();
@@ -43,33 +47,55 @@ function App() {
                 // save token in storage
                 localStorage.setItem('token', data.token)
                 setToken(data.token)
+                toast.success('🦄 Logged in!', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "light",
+                });
             })
     }
 
     return (
         <div className='container my-4'>
-            
+
+            <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
 
             {
                 loggedIn ?
-                <Profile setLoggedIn={setLoggedIn} setToken={setToken} />
-                :
-                <Tabs
-                id="controlled-tab-example"
-                activeKey={key}
-                onSelect={(k) => setKey(k)}
-                className="mb-3"
-            >
-                <Tab eventKey="login" title="Login">
-                    <LoginForm handleChange={handleChange} handleSubmit={handleSubmit} />
-                </Tab>
-                <Tab eventKey="register" title="Register now!">
-                    <RegisterForm />
-                </Tab>
-            </Tabs>
+                    <Profile setLoggedIn={setLoggedIn} setToken={setToken} />
+                    :
+                    <Tabs
+                        id="controlled-tab-example"
+                        activeKey={key}
+                        onSelect={(k) => setKey(k)}
+                        className="mb-3"
+                    >
+                        <Tab eventKey="login" title="Login">
+                            <LoginForm handleChange={handleChange} handleSubmit={handleSubmit} />
+                        </Tab>
+                        <Tab eventKey="register" title="Register now!">
+                            <RegisterForm />
+                        </Tab>
+                    </Tabs>
             }
 
-            
+
         </div>
     )
 }
--- Reactjs/login-register/src/components/profile.js
+++ Reactjs/login-register/src/components/profile.js
@@ -1,28 +1,56 @@
-import React, { useState,useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import jwt_decode from "jwt-decode";
+import { ToastContainer, toast } from 'react-toastify';
 import { Button } from 'react-bootstrap';
-function Profile({setLoggedIn,setToken}) {
-    let [user,setUser] = useState({})
+function Profile({ setLoggedIn, setToken }) {
+    let [user, setUser] = useState({})
 
-    useEffect(()=>{
+    useEffect(() => {
         let token = localStorage.getItem('token');
         var decoded = jwt_decode(token);
         setUser(decoded)
-    },[])
+    }, [])
 
-    const logoutHandler=(e)=>{
-        localStorage.removeItem("token") // remove from storage
-        setLoggedIn(false) // user not logged in
-        setToken("") // no token in state
+    const logoutHandler = (e) => {
+
+        toast.success('🦄 Logged out!', {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+        setTimeout(() => {
+            localStorage.removeItem("token") // remove from storage
+            setLoggedIn(false) // user not logged in
+            setToken("") // no token in state
+        }, 2000)
     }
 
-  return (
-    <div>
-    <h1>Profile Section</h1>
-    <h2>Welcome, {user.username}</h2>
-    <Button variant='danger' onClick={logoutHandler}>Logout</Button>
-    </div>
-  )
+    return (
+        <div>
+            <h1>Profile Section</h1>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
+            <h2>Welcome, {user.username}</h2>
+            <Button variant='danger' onClick={logoutHandler}>Logout</Button>
+        </div>
+    )
 }
 
 export default Profile
